8170c58ea743d111f93d5c4b11836a2ba61350af
Filippo <corti.filippo03@gmail.com>
2024-11-13 10:31:25 +0100

    Repository Cleanup

--- precompute_hdn_patches.sh
+++
@@ -1,4 +0,0 @@
-python train_hdn.py --cache_patches --dataset_name=jump_cell_painting
-python train_hdn.py --cache_patches --dataset_name=hagen
-python train_hdn.py --cache_patches --dataset_name=support
-python train_hdn.py --cache_patches --dataset_name=w2s
