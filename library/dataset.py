b775f70c969c276b0f2349f72efcb20942c6a7ba
Filippo <corti.filippo03@gmail.com>
2024-11-26 21:44:23 +0100

    Started working on Notebooks

--- library/dataset.py
+++ library/dataset.py
@@ -1,8 +1,10 @@
 # import argparse
-# import os
 # import sys
-# import aiohttp
-# import os
+import aiohttp
+import os
+
+from careamics_portfolio import PortfolioManager
+from pathlib import Path
 # import numpy as np
 # import yaml
 
@@ -11,13 +13,30 @@
 
 # Load Datasets
 
-def load_jump_dataset():
-    print("Loading Jump Dataset")
-    pass
+JUMP_URL = "https://zenodo.org/records/10912386/files/noisy.tiff?download=1"
 
-def load_bsd68_dataset():
+
+async def load_jump_dataset(path):
+    print("Loading Jump Dataset")
+    async with aiohttp.ClientSession() as session:
+        dir_path = os.path.dirname(path)
+        os.makedirs(dir_path, exist_ok=True)
+        async with session.get(JUMP_URL) as response:
+            # Write the content to the file in binary mode
+            with open(path, "wb") as f:
+                # Await and write the response content to the file
+                f.write(await response.read())
+    print("Jump Dataset Loaded at", path)
+
+
+def load_bsd68_dataset(root_path):
     print("Loading BSD68 Dataset")
-    pass
+    # instantiate data portfolio manage
+    portfolio = PortfolioManager()
+
+    # and download the data
+    files = portfolio.denoising.N2V_BSD68.download(root_path)
+    print("BSD68 Dataset Loaded at", root_path)
 
 # Split Datasets
 
