be72ce0b5c1719cd7c245e50c4e93391a3e5b6ea
Filippo <corti.filippo03@gmail.com>
2024-11-27 12:47:26 +0100

    Working on Notebooks

--- library/dataset.py
+++ library/dataset.py
@@ -1,19 +1,16 @@
-# import argparse
-# import sys
 import aiohttp
 import os
+import tifffile 
 
 from careamics_portfolio import PortfolioManager
-from pathlib import Path
 # import numpy as np
 # import yaml
 
-# import tifffile as tiff
 # import logging as log
 
-# Load Datasets
-
 JUMP_URL = "https://zenodo.org/records/10912386/files/noisy.tiff?download=1"
+
+# Load Datasets
 
 
 async def load_jump_dataset(path):
@@ -40,6 +37,19 @@ def load_bsd68_dataset(root_path):
 
 # Split Datasets
 
-def split_dataset():
-    pass
+def split_jump_dataset(path, split_ratio=0.8):
+    # Load and Split the Dataset
+    dataset = tifffile.imread(path)
+    print(f"Dataset size is {len(dataset)}")
+    split_idx = int(len(dataset) * split_ratio)
+    train, val = dataset[:split_idx], dataset[split_idx:]
+    print(f"Split the dataset at index {split_idx}/{len(dataset)}")
+
+    # Save the Split Datasets
+    dpath = path.replace('.tiff', f"_train.tiff")
+    tifffile.imwrite(dpath, data=train)
+    print(f"Train dataset saved to {dpath}")
+    dpath = path.replace('.tiff', f"_val.tiff")
+    tifffile.imwrite(dpath, data=val)
+    print(f"Validation dataset saved to {dpath}")
 
